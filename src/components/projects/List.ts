f5fdef17b34eded9443be9c23f5c26baf8cc9eef
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-08-18 00:13:49 +0200

    Worked on Carousel Page

--- src/components/projects/List.ts
+++ src/components/projects/List.ts
@@ -1,18 +1,56 @@
+import Project1 from './../../assets/project1.jpg';
+import Project2 from './../../assets/project2.jpg';
+import Project3 from './../../assets/project3.jpg';
+import Project4 from './../../assets/project4.jpg';
+
 export const List = [
     {
+        image: Project1,
         title: "Car Website",
         name: "Audi R8",
+        displayName1: "C",
+        displayName2: "A",
+        displayName3: "R",
+        displayName4: "S",
+        number: "01",
+        link:"https://www.audi.co.za/za/web/en/models/r8/r8-coupe-v10-performance-quattro.html",
     },
     {
+        image: Project2,
         title: "Car Website",
         name: "Audi R8",
+        displayName1: "M",
+        displayName2: "O",
+        displayName3: "V",
+        displayName4: "I",
+        displayName5: "E",
+        number: "02",
+        link: "https://www.imdb.com/title/tt7286456/",
+
     },
     {
+        image: Project3,
         title: "Car Website",
         name: "Audi R8",
+        displayName1: "S",
+        displayName2: "H",
+        displayName3: "O",
+        displayName4: "E",
+        displayName5: "S",
+        number: "03",
+        link:"https://www.nike.com/za/",
     },
     {
+        image: Project4,
         title: "Car Website",
         name: "Audi R8",
+        displayName1: "T",
+        displayName2: "R",
+        displayName3: "A",
+        displayName4: "V",
+        displayName5: "E",
+        displayName6: "L",
+        number: "04",
+        link:"https://www.travelstart.co.za/"
     },
 ];
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useRef, useState } from 'react'
 import '../projects/projects.css'
 import { List } from './List'
 import Car from '../../assets/car.jpg'
+import Project1 from '../../assets/Project1.jpg'
+import Project2 from '../../assets/Project2.jpg'
+import Project3 from '../../assets/Project3.jpg'
+import Project4 from '../../assets/Project4.jpg'
 
 function projects() {
   const [activeItem, setActiveItem] = useState(0);
@@ -33,20 +37,40 @@ function projects() {
     <>
     <section id='projects'>
     <div id='projectsContainer' className='flex h-full w-full items-center justify-center'>
-      <div className='w-[1200px] max-w-full'>
+      <div className='w-[1200px] max-w-full h-full max-h-full'>
         <ul className='group flex flex-col md:h-[640px] md:flex-row md:gap-[1.5%]'>
           {List.map((Lists, index) => (
-            <li onClick={() => setActiveItem(index)} aria-current={activeItem === index} className='relative md:w-[12%] md:[transition:width_var(--transition,200ms_ease-in)] md:hover:w-[12%] md:[&:not(:hover), &:not(:first),&:not(:last)]:group-hover:w-[7%] overflow-hidden [&[aria-current="true"]]:w-[60%] before:hidden md:before-block before:bg-red-300 before:top-0 before:bottom-0 before:left-[-10px] before:right-[-10px] before:absolute' key={Lists.name}>
-              <div className='listBox relative h-[100%] w-[98%] overflow-hidden rounded-4xl bg-[grey] mb-4'>
-              <img className=' absolute right-0 max-w-none h-auto w-[150px] md:h-[640px] md:w-[800px] left-1/2 top-1/2 md:-translate-x-1/2 -translate-y-1/2 object-cover grayscale' src={Car} alt={Lists.name} width={"800px"} height={"640px"} />
-              <div id='purpleglow' className={"inset-0 opacity-25 duration-300 before:absolute before:bottom-0 before:left-[-546px] before:right-0 before:top[-148px] before:z-10 before activeitem === index ? md:opacity-25 : md:opacity-0"}>
-                
+           <li id='contentContainer'
+           onClick={() => setActiveItem(index)}
+           aria-current={activeItem === index}
+           className={`relative md:w-[12%] md:[transition:width_var(--transition,200ms_ease-in)] md:hover:w-[12%] md:[&:not(:hover), &:not(:first),&:not(:last)]:group-hover:w-[7%] overflow-hidden [&[aria-current="true"]]:w-[60%] before:hidden md:before-block before:bg-red-300 before:top-0 before:bottom-0 before:left-[-10px] before:right-[-10px] before:absolute ${
+             activeItem === index ? 'active' : ''
+           }`}
+           key={Lists.name}
+         >
+           <div className='listBox relative h-[125px] md:h-[100%] w-[98%] overflow-hidden rounded-4xl bg-[#000000] mb-4'>
+             <img id='websiteImg'
+               className={`absolute right-0 max-w-none h-auto w-[150px] md:h-[640px] md:w-[800px] left-1/2 top-1/2 md:-translate-x-1/2 -translate-y-1/2 object-cover grayscale ${
+                 activeItem === index ? 'opacity-1' : 'opacity-0'
+               }`}
+               src={Lists.image}
+               alt={Lists.name}
+               width={800}
+               height={640}
+             />
+              <div id='projectName'>
+                <h1 id='number'>{Lists.number}</h1>
+                <h1><span className='letter'>{Lists.displayName1}</span></h1>
+                <h1 id='letter'><span className='letter'>{Lists.displayName2}</span></h1>
+                <h1 id='letter'><span className='letter'>{Lists.displayName3}</span></h1>
+                <h1 id='letter'><span className='letter'>{Lists.displayName4}</span></h1>
+                <h1 id='letter'><span className='letter'>{Lists.displayName5}</span></h1>
+                <h1 id='letter'><span className='letter'>{Lists.displayName6}</span></h1>
               </div> 
-              <div className={`left-8 top-8 p-10 md:p-10 w-[800px] transition-[transform,opacity] ${
+              <div className={`flex justify-center left-8 top-8 -translate-y-36 p-10 md:p-10 w-[800px] transition-[transform,opacity] ${
               activeItem === index ? 'md:translate-x-0 md:opacity-100' : 'md:translate-x-4 md:opacity-0'
               }`}>
-                <p className='text-sm md:text-lg uppercase text-[#FFFFFF]'>{Lists.title}</p>
-                <p className='text-lg uppercase text-[#FFFFFF] md:text-4xl'>{Lists.name}</p>
+                <a href={Lists.link}><button id='linkBtn' className='text-lg uppercase text-[#FFFFFF] md:text-4xl'>Explore</button></a>
               </div>
               </div>
             </li>
