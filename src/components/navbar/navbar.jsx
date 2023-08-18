0d5322b762b3a2dc8321be6595575a185d8d349e
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-08-18 21:22:52 +0200

    Updated the NavBar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,77 +1,60 @@
-import React, {useState} from 'react'
-import '../navbar/navbar.css'
-import {AiFillCloseCircle} from 'react-icons/ai'
-import {BiMenu} from 'react-icons/bi'
+import React, { useState } from 'react';
+import '../navbar/navbar.css';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { BiMenu } from 'react-icons/bi';
 
 function Navbar() {
-    const [active, setActive] = useState('navBar')
-    // Function to toggle the navbar
-    const showNav = ()=>{
-        setActive('navBar activeNavbar')
-    }
-    // Function to remove the navbar
-    const removeNavbar = ()=>{
-        setActive('navBar')
-    }
+  const [active, setActive] = useState('navBar');
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const showNav = () => {
+    setActive('navBar activeNavbar');
+    setSelectedItem(null);
+  };
+
+  const removeNavbar = () => {
+    setActive('navBar');
+  };
+
   return (
     <>
-    <section className='navBarSection'>
+      <section className='navBarSection'>
         <header className='header flex'>
+          <div className='logoDiv'>
+            <a href="#" className='logo flex'>
+              <h1>Logo</h1>
+            </a>
+          </div>
 
-            <div className='logoDiv'>
-                <a href="#" className='logo flex'>
-                <h1>Logo</h1>
-                </a>
+          <div className={active}>
+            <ul className='navLists flex'>
+              {['Home', 'About', 'Education', 'Experience', 'Skills', 'Projects', 'Blog', 'Contact'].map(
+                (item) => (
+                  <li
+                    key={item}
+                    className={`navItem ${selectedItem === item ? 'selected' : ''}`}
+                    onClick={() => setSelectedItem(item)} // Set selected item on click
+                  >
+                    <a href={`#${item.toLowerCase()}`} className='navLink'>
+                      {item}
+                    </a>
+                  </li>
+                )
+              )}
+            </ul>
+
+            <div onClick={removeNavbar} className='closeNavbar'>
+              <AiFillCloseCircle className='icon' />
             </div>
+          </div>
 
-            <div className={active}>
-                <ul className='navLists flex'>
-                    <li className='navItem'>
-                        <a href="#home" className='navLink'>Home</a>
-                    </li>
-
-                    <li className='navItem'>
-                        <a href="#about" className='navLink'>About</a>
-                    </li>
-
-                    <li className='navItem'>
-                        <a href="#educationandexperience" className='navLink'>Education</a>
-                    </li>
-
-                    <li className='navItem'>
-                        <a href="#experience" className='navLink'>Experience</a>
-                    </li>
-
-                    <li className='navItem'>
-                        <a href="#skills" className='navLink'>Skills</a>
-                    </li>
-
-                    <li className='navItem'>
-                        <a href="#projects" className='navLink'>Projects</a>
-                    </li>
-
-                    <li className='navItem'>
-                        <a href="#blog" className='navLink'>Blog</a>
-                    </li>
-
-                    <li className='navItem'>
-                        <a href="#contact" className='navLink'>Contact</a>
-                    </li>
-                </ul>
-
-                <div onClick={removeNavbar} className='closeNavbar'>
-                    <AiFillCloseCircle className="icon"/>
-                </div>
-            </div>
-
-            <div onClick={showNav} className="toggleNavbar">
-                <BiMenu className="icon"/>
-            </div>
-
+          <div onClick={showNav} className='toggleNavbar'>
+            <BiMenu className='icon' />
+          </div>
         </header>
-    </section>
+      </section>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
