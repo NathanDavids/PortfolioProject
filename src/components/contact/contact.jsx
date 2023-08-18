848374b328c0a9dbb41bcc44f87419e2b2b87cf5
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-08-18 14:58:27 +0200

    Worked on Contact Page

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,12 +1,71 @@
 import React from 'react'
 import '../contact/contact.css'
+import {FaMapMarkedAlt} from 'react-icons/fa'
+import {MdEmail} from 'react-icons/md'
+import {BsFillTelephoneFill} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsWhatsapp} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
 
 function contact() {
   return (
     <>
     <section id='contact'>
-    <div id='contactContainer'>
-
+    <div id='contactContainer' classname="flex w-full min-h-screen max-w-4xl p-8 rounded-xl shadow-lg">
+      <div className="form flex flex-col md:flex-row md:space-x-6 md:space-y-0 space-y-6 sm:p-12 bg-[grey] w-full max-w-7xl p-8 rounded-xl shadow-lg text-white">
+        <div className="flex flex-col space-y-8 justify-between">
+          <div>
+            <h1 className="flex justify-center font-bold text-4xl tracking-wide"> Contact Me </h1>
+            <p className="flex justify-center text-center pt-2 text-white-100 text-sm"> It would be amazing to chat, exchange ideas, and explore potential collaboration opportunities.
+              Let's schedule a call or grab a coffee sometime soon. Looking forward to hearing from you and embarking on an exciting journey of creativity together. </p>
+          </div>
+          <div className="flex flex-col items-center  space-y-4">
+            <div className="inline-flex space-x-2 items-center">
+              <FaMapMarkedAlt className="icon text-white-300 text-xl"/>
+              <span>Cape Town, South Africa</span>
+            </div>
+            <div className="inline-flex space-x-2 items-center">
+              <MdEmail className="icon text-white-300 text-xl"/>
+              <span>nathandavids935@gmail.com</span>
+            </div>
+            <div className="inline-flex space-x-2 items-center">
+              <BsFillTelephoneFill className="icon text-white-300 text-xl"/>
+              <span>+27 61 679 2852</span>
+            </div>
+          </div>
+          <div className="flex justify-center text-4xl tracking-wide">
+            <h2>Socials</h2>
+          </div>
+          <div className="flex justify-center justify-around space-x-4 text-4xl">
+            <a href=""><BsLinkedin/></a>
+            <a href=""><BsGithub/></a>
+            <a href=""><BsWhatsapp/></a>
+            <a href=""><BsInstagram/></a>
+          </div>
+          <div className="bg-black rounded-xl shadow-lg p-8">
+            <form action="" className="flex flex-col space-y-4">
+              <div>
+                <label htmlFor="" className="text-sm"> Name </label>
+                <input type="text" placeholder="Name" className="text-gray-600 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-red-600" />
+              </div>
+              <div>
+                <label htmlFor="" className="text-sm"> Email </label>
+                <input type="email" placeholder="Email" className="text-gray-600 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-red-600" />
+              </div>
+              <div>
+                <label htmlFor="" className="text-sm"> Phone Number </label>
+                <input type="number" placeholder="Phone Number" className="text-gray-600 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-red-600" />
+              </div>
+              <div>
+                <label htmlFor="" className="text-sm"> Message </label>
+                <textarea type="text" placeholder="Message" rows="4" className="text-gray-600 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-red-600" />
+              </div>
+              <button className="inline-block self-center bg-gray-700 text-white font-bold rounded-lg px-6 py-2 uppercase text-sm"> Send Message </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
     </section>
     </>
