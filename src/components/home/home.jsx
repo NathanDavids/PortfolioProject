e8b0bf141a904203605318299f7f216d3c42cc97
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-08-21 13:46:20 +0200

    Added all Home Content (structure)

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import '../home/home.css'
 import HomePicture from '../../assets/HomePicture.png'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
+import { IoLogoWhatsapp } from 'react-icons/io'
+import { MdLightMode } from 'react-icons/md'
 
 function home() {
   return (
@@ -11,6 +15,20 @@ function home() {
           <div id='nameContainer'>
             <h1>Nathan <span>Davids</span> Nathan <span> Davids</span></h1>         
           </div>
+          <div id='homeImgContainer'>
+            <img src={HomePicture} alt="" className='homeImg'/>
+          </div>
+        </div>
+        <div id='homeTextContainer'>
+          <h1>
+          "Hard Work Beats Talent When Talent <span className='quoteRed'> Doesn't Work Hard"</span>
+          </h1>
+        </div>
+        <div id='homeSocialsContainer'>
+          <BsLinkedin className='icon'/>
+          <BsGithub className='icon'/>
+          <IoLogoWhatsapp className='icon'/>
+          <MdLightMode className='icon'/>
         </div>
       </div>
     </section>
