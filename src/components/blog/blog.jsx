b660fba17e105f9d3c8d10889e70796e22ad9fd8
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-08-23 14:02:20 +0200

    Added all the content for the containers

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -5,19 +5,22 @@ import PostTwo from '../../assets/Two.png';
 import PostThree from '../../assets/Three.png';
 
 function Blog() {
-  const [expandedPost, setExpandedPost] = useState(null);
+  const [expandedPosts, setExpandedPosts] = useState(Array(3).fill(false)); // Initialize with the number of posts you have
 
   const handleExpandPost = (postIndex) => {
-    setExpandedPost(postIndex);
+    const newExpandedPosts = expandedPosts.map((value, index) =>
+      index === postIndex ? !value : false
+    );
+    setExpandedPosts(newExpandedPosts);
   };
 
   return (
     <>
       <section id='blog'>
-      <div id='blogContainer' className='flex flex-col h-full w-full'>
-        <div className="blogImage">
-          <h1 className='blogHeading'> BLOG </h1>
-        </div>
+        <div id='blogContainer' className='flex flex-col h-full w-full'>
+          <div className="blogImage">
+            <h1 className='blogHeading'> BLOG </h1>
+          </div>
           <div className="blogPostContainer grid ml-20 md:ml-0 lg:ml-0 md:flex md:flex-wrap justify-center items-center h-full w-full overflow-hidden gap-4 md:gap-8">
             {[
               { image: PostOne, heading: "Working World", text: "The start of my journey into becoming an adult..." },
@@ -26,21 +29,40 @@ function Blog() {
             ].map((post, index) => (
               <div
                 key={index}
-                className={`postContainer items-center w-[70%] h-[70%] md:w-1/1 md:h-[32%] lg:w-1/4 lg:h-[75%] xl:w-1/4  bg-white flex flex-col space-y-3 ${expandedPost === index ? 'expanded' : ''}`}
+                className={`postContainer items-center w-[70%] h-[70%] md:w-1/1 md:h-[32%] lg:w-1/4 lg:h-[75%] xl:w-1/4 bg-white rounded-lg p-4 flex flex-col space-y-3 ${expandedPosts[index] ? 'expanded' : ''}`}
               >
-                {expandedPost !== index ? (
+                {expandedPosts[index] ? (
                   <>
+                    {/* Expanded content */}
+                    <h2 className='expandedHeading'>{post.heading}</h2>
+                    <p>
+                      {/* Add additional text or components specific to this post */}
+                      {/* For example: */}
+                      {index === 0 && (
+                        <>
+                          <p className='text-center'>The transition into adulthood coincided with a pivotal opportunity at Innovo Networks. As a PABX Technician, my role encompassed not only technical support but also marked the commencement of practical experience in the professional realm. Innovo Networks provided the platform to bridge the gap between theoretical knowledge and hands-on application—a foundational step in my journey into the world of work.</p>
+                        </>
+                      )}
+                      {index === 1 && (
+                        <>
+                          <p className='text-center mt-5'>The awaited opportunity has arrived, presented by CapaCiTi—a chance to upskill in full-stack development over the next 6 months. This is the gateway I've been anticipating, where aspirations meet action. CapaCiTi's program offers a dedicated timeframe to hone expertise, a transformative journey into a new realm of skills and knowledge.</p>
+                        </>
+                      )}
+                      {index === 2 && (
+                        <>
+                          <p className='text-center'>Now, after 6 months, the next chapter unfolds—phase 2 commences. This time, it's at younglings, where the upcoming year will be dedicated to crafting full-stack websites. This stretch offers not just skill refinement, but a chance to immerse myself in practical application. As the year unfolds, the goal comes into focus: the aspiration to secure a permanent position by the journey's end.</p>
+                        </>
+                      )}
+                    </p>
+                  </>
+                ) : (
+                  <>
+                    {/* Short content */}
                     <img src={post.image} alt="" className="w-full mb-2" />
                     <h2 className='postHeading text-center text-xl font-semibold mb-2'>{post.heading}</h2>
                     <br />
                     <p className='postText text-sm mb-4 m-2'>{post.text}</p>
                     <button onClick={() => handleExpandPost(index)} className='inline-block self-center px-8 py-4 bg-[#000000] text-white px-4 py-2 rounded-md hover:bg-[#D9171F]'> Read More </button>
-                  </>
-                ) : (
-                  <>
-                    {/* Content to show when expanded */}
-                    <p>Additional content for {post.heading}</p>
-                    {/* You can add any other content here */}
                   </>
                 )}
               </div>
