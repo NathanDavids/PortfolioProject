97e67110ee67393acd2ce62703e00eb61ec68b5e
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-08-25 10:36:03 +0200

    Added the different Mode Logo's

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import '../navbar/navbar.css';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { BiMenu } from 'react-icons/bi';
+import LogoDark from '../../assets/Logo_Dark.png'
+import LogoLight from '../../assets/Logo_Light.png'
 
 function Navbar({ isDarkMode }) {
   const [active, setActive] = useState('navBar');
@@ -22,7 +24,7 @@ function Navbar({ isDarkMode }) {
         <header className={`header flex ${isDarkMode ? 'dark' : 'light'}`}>
           <div className='logoDiv'>
             <a href="#home" className='logo flex'>
-              
+              <img src={isDarkMode ? LogoDark : LogoLight} alt="" />
             </a>
           </div>
 
