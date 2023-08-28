ff0f9d7d5b2ba6911dcc1b31e3f2f217e9ada0ad
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-08-28 15:01:26 +0200

    Worked on the Contact Page (firebase)

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,31 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const sgMail = require("@sendgrid/mail");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const SENDGRID_API_KEY =
+  "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";
+sgMail.setApiKey(SENDGRID_API_KEY);
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendEmail = functions.https.onCall(async (data, context) => {
+  // Check if the requester is authenticated
+  if (!context.auth) {
+    return {error: "Not authorized."};
+  }
+
+  const msg = {
+    to: data.to,
+    from: "nathandavids935@gmail.com", // Replace with your email address
+    subject: data.subject,
+    text: data.text,
+  };
+
+  try {
+    await sgMail.send(msg);
+    return {message: "Email sent successfully!"};
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return {error: "Error sending email."};
+  }
+});
