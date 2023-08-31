8d3dc28c1df2f505a16042bb5294e6a42f35b1f7
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-08-31 14:06:49 +0200

    Added the Footer Light Mode

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,13 +5,13 @@ function footer({ isDarkMode }) {
   return (
     <>
     <section id='footer' className={isDarkMode ? 'dark' : 'light'}>
-    <div id="footerContainer" className="flex h-[50px] w-full ml-12 items-center">
+    <div id="footerContainer" className={`flex h-[50px] w-full ml-12 items-center ${isDarkMode ? 'dark' : 'light'}`}>
     <p id='copyrightText' className="flex-grow text-center w-full pl-12">COPYRIGHT © NATHAN DAVIDS | 2023 </p>
     
     {/* Empty space to push the button to the right */}
     <div className="flex-grow"></div>
     
-    <a href="#home"><button id='copyrightBtn' className="text-white py-2 px-5 mr-10 rounded-xl"> ↑ </button></a>
+    <a href="#home"><button id='copyrightBtn' className={`text-white py-2 px-5 mr-10 rounded-xl ${isDarkMode ? 'dark' : 'light'}`}> ↑ </button></a>
     </div>
     </section>
     </>
