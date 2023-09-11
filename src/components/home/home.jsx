775a36d96dec025e0d2ca662ebc70de1f12b2ec2
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-09-11 10:30:07 +0200

    Added Comments to the Home JSX and CSS

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -19,15 +19,17 @@ import { MdLightMode } from 'react-icons/md'
 import CV from '../../assets/NathanDavids_CV.pdf'
 
 function home() {
+
+  /* The following activates dark and light mode */
   const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('theme') === 'dark');
 
   const toggleDarkMode = () => {
     const newTheme = isDarkMode ? 'light' : 'dark';
     localStorage.setItem('theme', newTheme);
     setIsDarkMode(newTheme === 'dark');
-    // You might also want to emit an event or use a context to inform other components about the theme change.
   };
 
+  /* The following provides the Typing effect to the quote */
   useEffect(() => {
     // Initialize Typed within the useEffect hook
     const typed = new Typed(".multiple-text", {
@@ -49,6 +51,9 @@ function home() {
     <Navbar isDarkMode={isDarkMode}/>
     <section id='home' className={isDarkMode ? 'dark' : 'light'}>
       <div id='homeContainer'>
+
+        {/* Name and Image Container */}
+
         <div id='mainContainer'>
           <div id='nameContainer'>
             <h1 className='name'>Nathan <span className='surnameText'>Davids</span> Nathan <span className='surnameText'> Davids </span>
@@ -60,22 +65,33 @@ function home() {
             <img src={HomePicture} alt="" className='homeImg'/>
           </div>
         </div>
+
+        {/* Quote Container */}
+
         <div id='homeTextContainer'>
           <h1 id='homeQuote'>
           "Hard Work Beats Talent When Talent <span className='multiple-text'> Doesn't Work Hard"</span>
           </h1>
         </div>
+
+        {/* Social Links Container */}
+
         <div id='homeSocialsContainer'>
           <a href="https://www.linkedin.com/in/nathan-davids-7a857b233/" target="_blank" rel="noopener noreferrer"><BsLinkedin className='icon hover:text-[#D9171F]'/></a>
           <a href="https://github.com/NathanDavids" target="_blank" rel="noopener noreferrer"><BsGithub className='icon hover:text-[#D9171F]'/></a>
           <a href="https://wa.link/38j3gp" target="_blank" rel="noopener noreferrer"><IoLogoWhatsapp className='icon hover:text-[#D9171F]'/></a>
           <button onClick={toggleDarkMode}><MdLightMode className='icon hover:text-[#D9171F]'/> </button>
         </div>
+
+        {/* CV Button Container */}
+
         <div id='btnContainer'>
           <a href={CV} target="_blank" rel="noopener noreferrer"><button id='downloadBtn' >Download CV</button></a>
           </div>
       </div>
     </section>
+
+    {/* This allows the Dark and Light Mode state to pass through all the other components (to trigger the same mode simultaneously) */}
     <About isDarkMode={isDarkMode} />
     <EducationAndExperience isDarkMode={isDarkMode} />
     <Experience isDarkMode={isDarkMode} />
