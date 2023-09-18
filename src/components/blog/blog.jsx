0b3b215c6ab0152fa00ab109d3971f3d62c8f3af
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-09-18 14:20:23 +0200

    Updated Mobile & Tablet View (Blog)

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -30,13 +30,13 @@ function Blog({ isDarkMode }) {
           {/* The following contains the Blog Post Contianers */}
           <div className={`blogPostContainer grid md:flex justify-center items-center h-full w-full overflow-hidden gap-4 md:gap-8 ${isDarkMode ? 'dark' : 'light'}`}>
             {[
-              { image: PostOne, heading: "Working World", text: "The start of my journey into becoming an adult..." },
+              { image: PostOne, heading: "Working World", text: "The start of my journey into adulthood..." },
               { image: PostTwo, heading: "New Beginnings", text: "The opportunity I've been waiting for..." },
               { image: PostThree, heading: "One Step Closer", text: "6 months later and phase 2 has begun..." },
             ].map((post, index) => (
               <div
                 key={index}
-                className={`postContainer overflow-hidden items-center w-full md:w-[30%] lg:w-full xl:w-[35%] bg-white rounded-lg p-4 flex flex-col space-y-3 ${
+                className={`postContainer overflow-hidden items-center w-full sm:w-[30%] sm:-h[68%] md:w-[30%] md:-h[68%] lg:w-full lg-h[80%] xl:w-[35%] bg-white rounded-lg p-4 flex flex-col space-y-3 ${
                 expandedPosts[index] ? 'expanded' : ''
               } ${isDarkMode ? 'dark' : 'light'}`}
               >
@@ -47,17 +47,17 @@ function Blog({ isDarkMode }) {
                     <p>
                       {index === 0 && (
                         <>
-                          <p className='text-center mt-5'>The transition into adulthood coincided with a pivotal opportunity at Innovo Networks. As a PABX Technician, my role encompassed not only technical support but also marked the commencement of practical experience in the professional realm. Innovo Networks provided the platform to bridge the gap between theoretical knowledge and hands-on application—a foundational step in my journey into the world of work.</p>
+                          <p className='extendedContent text-center mt-5'>The transition into adulthood coincided with a pivotal opportunity at Innovo Networks. As a PABX Technician, my role encompassed not only technical support but also marked the commencement of practical experience in the professional realm. Innovo Networks provided the platform to bridge the gap between theoretical knowledge and hands-on application—a foundational step in my journey into the world of work.</p>
                         </>
                       )}
                       {index === 1 && (
                         <>
-                          <p className='text-center mt-5'>The awaited opportunity has arrived, presented by CapaCiTi—a chance to upskill in full-stack development over the next 6 months. This is the gateway I've been anticipating, where aspirations meet action. CapaCiTi's program offers a dedicated timeframe to hone expertise, a transformative journey into a new realm of skills and knowledge.</p>
+                          <p className='extendedContent text-center mt-5'>The awaited opportunity has arrived, presented by CapaCiTi—a chance to upskill in full-stack development over the next 6 months. This is the gateway I've been anticipating, where aspirations meet action. CapaCiTi's program offers a dedicated timeframe to hone expertise, a transformative journey into a new realm of skills and knowledge.</p>
                         </>
                       )}
                       {index === 2 && (
                         <>
-                          <p className='text-center mt-5'>Now, after 6 months, the next chapter unfolds—phase 2 commences. This time, it's at younglings, where the upcoming year will be dedicated to crafting full-stack websites. This stretch offers not just skill refinement, but a chance to immerse myself in practical application. As the year unfolds, the goal comes into focus: the aspiration to secure a permanent position by the journey's end.</p>
+                          <p className='extendedContent text-center mt-5'>Now, after 6 months, the next chapter unfolds—phase 2 commences. This time, it's at younglings, where the upcoming year will be dedicated to crafting full-stack websites. This stretch offers not just skill refinement, but a chance to immerse myself in practical application. As the year unfolds, the goal comes into focus: the aspiration to secure a permanent position by the journey's end.</p>
                         </>
                       )}
                     </p>
